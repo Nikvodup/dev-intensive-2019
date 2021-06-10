f92b9ab972f03efa50262b809d37894fb48075be
Alex <ransbach25@gmail.com>
2021-06-10 09:38:00 +0300

    hometask_3

--- app/src/main/java/ru/skill_branch/dev_intensive/MainActivity.kt
+++ app/src/main/java/ru/skill_branch/dev_intensive/MainActivity.kt
@@ -4,6 +4,7 @@ package ru.skill_branch.dev_intensive
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -25,15 +26,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("M_MainActivity","onCreate")
 
        //  benderImage = findViewById(R.id.iv_bender) as ImageView
            benderImage = iv_bender
-       textTxt = tv_text
+        textTxt = tv_text
         messageET = et_message
         sendBtn = iv_sender
 
-        benderObj = Bender()
+
+        val status = savedInstanceState?.getString("STATUS")?:Bender.Status.NORMAL.name
+        val question=savedInstanceState?.getString("QUESTION")?:Bender.Question.NAME.name
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        Log.d("M_MainActivity","onCreate ${status} ${question}")
+
+        val (r,g,b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
 
         textTxt.text = benderObj.askQuestion()
 
@@ -69,6 +77,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onDestroy(){
         super.onDestroy()
         Log.d("M_MainActivity","onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState:Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION",benderObj.question.name)
+        Log.d("M_MainActivity","onSaveInstanceState ${benderObj.question.name} ${benderObj.status.name}")
     }
 
     override fun onClick(v: View?) {
--- app/src/main/java/ru/skill_branch/dev_intensive/model/Bender.kt
+++ app/src/main/java/ru/skill_branch/dev_intensive/model/Bender.kt
@@ -13,8 +13,13 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
 
     fun listenAnswer(answer:String):Pair<String,Triple<Int,Int,Int>>{
    return if(question.answers.contains(answer)){
-       "Отлично - Это правильный ответ!" to status.color
-   } else{ "Это не правильный ответ!" to status.color}
+     question=question.nextQuestion()
+       "Отлично - Это правильный ответ! \n ${question.question}" to status.color
+   } else{
+        status=status.nextStatus()
+       "Это не правильный ответ! \n ${question.question}" to status.color
+
+   }
     }
 
 
@@ -22,16 +27,38 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0))
+        CRITICAL(Triple(255,255,0));
+
+        fun nextStatus():Status{
+         return   if(this.ordinal<values().lastIndex){
+             values()[this.ordinal+1]
+         }else{
+             values()[0]
+         }
+        }
     }
 
     enum class Question(val question:String,val answers:List<String>){
-        NAME("Как меня зовут?",listOf("бендер","bender")),
-        PROFESSION("Назови мою профессию.",listOf("сгибальщик","bender")),
-        MATERIAL("Из чего я сделан?",listOf("метал","дерево","metal","wood","iron")),
-        BDAY("Когда меня создали?",listOf("2993")),
-        SERIAL("Мой серийный номер?",listOf("91234476")),
-        IDLE("На этом все, вопросов больше нет",listOf())
+        NAME("Как меня зовут?",listOf("бендер","bender")){
+               override fun nextQuestion():Question=PROFESSION
+        },
+        PROFESSION("Назови мою профессию.",listOf("сгибальщик","bender")){
+              override fun nextQuestion():Question= MATERIAL
+       },
+        MATERIAL("Из чего я сделан?",listOf("метал","дерево","metal","wood","iron")){
+            override fun nextQuestion():Question=BDAY
+       },
+        BDAY("Когда меня создали?",listOf("2993")){
+            override fun nextQuestion():Question=SERIAL
+     },
+        SERIAL("Мой серийный номер?",listOf("91234476")){
+            override fun nextQuestion():Question=IDLE
+       },
+        IDLE("На этом все, вопросов больше нет",listOf()){
+            override fun nextQuestion():Question=IDLE
+        };
+
+        abstract fun nextQuestion():Question
     }
 
 
